1478d3fbe389eb0caf5d4b4383281c9c975db4d6
Yann Le Vagueres <yann.lv@live.fr>
2024-04-29 13:58:31 +0200

    Switched to latest esp-hal

--- src/main.rs
+++ src/main.rs
@@ -5,29 +5,44 @@
 // Async cool stuff
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
-use esp32_hal::{self, embassy};
+use esp_hal::{self, embassy, gpio::AnyPin, Blocking};
 use defmt_rtt as _; // global logger
 
 // ESP basic stuff
-use esp32_hal::{clock::ClockControl, gpio::{OpenDrain, Output}, peripherals::Peripherals, prelude::*, IO, reset};
-use esp_backtrace as _;
+use esp_hal::{clock::ClockControl, gpio::{OpenDrain, Output, IO}, peripherals::Peripherals, prelude::*, reset};
 use esp_println::println;
 use core::cell::RefCell;
 
 // button interrupt
-use esp32_hal::gpio::{Gpio18, Gpio5, Input, PullUp};
-use embedded_hal_async::digital::Wait;
+use esp_hal::gpio::{Input, PullUp};
 
 // I2C peripherals
 use static_cell::StaticCell;
-use esp32_hal::{peripherals::I2C0, i2c::I2C};
+use esp_hal::{peripherals::I2C0, i2c::I2C};
 use embassy_sync::blocking_mutex::{raw::NoopRawMutex, NoopMutex};
 use embassy_embedded_hal::shared_bus::blocking::i2c::I2cDevice;
 use ds323x::{Ds323x, Rtcc}; // RTC
 use tea5767::defs::*; // Radio
+
 // type definition abstraction
+type I2CModeESP = Blocking;
 type I2CRawMutex = NoopRawMutex;
-type I2cDeviceESP<T> = I2cDevice<'static,I2CRawMutex, I2C<'static, T>>;
+type I2CMutex<T> = NoopMutex<T>;
+type I2CTypeESP<T> = I2C<'static, T, I2CModeESP>;
+type I2CDeviceESP<T> = I2cDevice<'static,I2CRawMutex, I2C<'static, T, I2CModeESP>>;
+
+
+/* ========================== *\
+|         PANIC HANDLER        |
+\* ========================== */
+
+// dev profile: easier to debug panics; can put a breakpoint on `rust_begin_unwind`
+#[cfg(debug_assertions)]
+use panic_halt as _;
+// release profile: minimize the binary size of the application
+#[cfg(not(debug_assertions))]
+use panic_abort as _;
+
 
 /* ========================== *\
 |            TASKS             |
@@ -35,7 +50,7 @@ type I2cDeviceESP<T> = I2cDevice<'static,I2CRawMutex, I2C<'static, T>>;
 
 
 #[embassy_executor::task]
-async fn rtc_task(con: I2cDeviceESP<I2C0>) -> ! {
+async fn rtc_task(con: I2CDeviceESP<I2C0>) -> ! {
     // no async implem for DS232x with latest embedded hal
     let mut rtc = Ds323x::new_ds3231(con);
 
@@ -50,7 +65,7 @@ async fn rtc_task(con: I2cDeviceESP<I2C0>) -> ! {
 }
 
 #[embassy_executor::task]
-async fn radio_task(con: I2cDeviceESP<I2C0>) {
+async fn radio_task(con: I2CDeviceESP<I2C0>) {
     let mut radio_tuner = TEA5767::new(
         con,
         107.0,
@@ -80,7 +95,7 @@ async fn radio_task(con: I2cDeviceESP<I2C0>) {
 
     embassy::init(
         &clocks,
-        esp32_hal::timer::TimerGroup::new(peripherals.TIMG0, &clocks)
+        esp_hal::timer::TimerGroup::new_async(peripherals.TIMG0, &clocks)
     );
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
@@ -89,15 +104,15 @@ async fn radio_task(con: I2cDeviceESP<I2C0>) {
     // I2C setup
     let sda = p.gpio21;
     let scl = p.gpio22;
-    let i2c = esp32_hal::i2c::I2C::new(peripherals.I2C0, sda, scl, 400_u32.kHz(), &clocks);
-    static I2C_BUS: StaticCell<NoopMutex<RefCell<I2C<'static, I2C0>>>> = StaticCell::new();
-    let i2c_bus = I2C_BUS.init(NoopMutex::new(RefCell::new(i2c)));
+    let i2c = I2C::new(peripherals.I2C0, sda, scl, 400_u32.kHz(), &clocks, None);
+    static I2C_BUS: StaticCell<I2CMutex<RefCell<I2CTypeESP<I2C0>>>> = StaticCell::new();
+    let i2c_bus = I2C_BUS.init(I2CMutex::new(RefCell::new(i2c)));
     let rtc_device = I2cDevice::new(i2c_bus);
     let radio_device = I2cDevice::new(i2c_bus);
 
     // led setup
     let mut led = p.gpio5.into_open_drain_output();
-    led.set_low().unwrap();
+    led.set_low();
 
     // button setup
     let button = p.gpio18.into_pull_up_input();
@@ -105,7 +120,7 @@ async fn radio_task(con: I2cDeviceESP<I2C0>) {
     spawner.spawn(radio_task(radio_device)).unwrap();
     spawner.spawn(rtc_task(rtc_device)).unwrap();
 
-    btnled_task(button, led).await
+    btnled_task(button.into(), led.into()).await
 }
 
 
@@ -114,13 +129,13 @@ async fn radio_task(con: I2cDeviceESP<I2C0>) {
 \* ========================== */
 
 
-async fn btnled_task(mut button: Gpio18<Input<PullUp>>, mut led: Gpio5<Output<OpenDrain>>) -> ! {
+async fn btnled_task(mut button: AnyPin<Input<PullUp>>, mut led: AnyPin<Output<OpenDrain>>) -> ! {
     loop {
-        if button.wait_for_any_edge().await.is_ok() {
-            let state = button.is_low().unwrap();
-            println!("State : {}", &state);
+        button.wait_for_any_edge().await;
 
-            led.set_state(state.into()).unwrap();
-        }
+        let state: bool = button.is_low();
+        println!("State : {}", &state);
+
+        led.set_state(state);
     }
 }
